92584272cd5a3f91099a7b7103320fcf7047ef8a
My Kim Bui <mykimbuibui@gmail.com>
2019-03-30 16:55:32 +0100

    drop that

--- fruity.js
+++ fruity.js
@@ -1,140 +1,142 @@
-'use strict';
-
-var initScene, render, _boxes = [], spawnBox, loader,
-renderer, scene, ground_material, ground, light, camera;
+'use strict'
+let initScene, render, _boxes = [], spawnBox, loader,
+renderer, scene, ground_material, ground, light, camera, group
 
 initScene = function() {
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMapSoft = true;
-  document.getElementById( 'viewport' ).appendChild( renderer.domElement );
+  renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  renderer.shadowMap.enabled = true
+  renderer.shadowMapSoft = true
+  document.getElementById( 'viewport' ).appendChild( renderer.domElement )
 
-  scene = new Physijs.Scene;
-  scene.background = new THREE.Color('#BBCDD3');
-  scene.setGravity(new THREE.Vector3( 0, -30, 0 ));
+  scene = new Physijs.Scene
+  scene.background = new THREE.Color('#BBCDD3')
+  scene.setGravity(new THREE.Vector3( 0, -30, 0 ))
   scene.addEventListener(
     'update',
     function() {
-      scene.simulate( undefined, 1 );
+      scene.simulate( undefined, 1 )
     }
-    );
+    )
 
   camera = new THREE.PerspectiveCamera(
     35,
     window.innerWidth / window.innerHeight,
     1,
     1000
-    );
-  camera.position.set( 0, 0, -50 );
-  camera.lookAt( scene.position );
-  scene.add( camera );
+    )
+  camera.position.set( 0, 0, -50 )
+  camera.lookAt( scene.position )
+  scene.add( camera )
 
-  light = new THREE.DirectionalLight( 0xFFFFFF );
-  light.position.set( 20, 40, -15 );
-  light.target.position.copy( scene.position );
-  light.castShadow = true;
-  light.shadow.camera.left = -60;
-  light.shadow.camera.top = -60;
-  light.shadow.camera.right = 60;
-  light.shadow.camera.bottom = 60;
-  light.shadow.camera.near = 20;
-  light.shadow.camera.far = 200;
+  light = new THREE.DirectionalLight( 0xFFFFFF )
+  light.position.set( 20, 40, -15 )
+  light.target.position.copy( scene.position )
+  light.castShadow = true
+  light.shadow.camera.left = -60
+  light.shadow.camera.top = -60
+  light.shadow.camera.right = 60
+  light.shadow.camera.bottom = 60
+  light.shadow.camera.near = 20
+  light.shadow.camera.far = 200
   light.shadow.bias = -.0001
-  light.shadow.mapSize.width = light.shadow.mapSize.height = 2048;
-  light.shadow.darkness = .1;
-  scene.add( light );
+  light.shadow.mapSize.width = light.shadow.mapSize.height = 2048
+  light.shadow.darkness = .1
+  scene.add( light )
 
-  loader = new THREE.TextureLoader();
+  loader = new THREE.TextureLoader()
 
   ground_material = Physijs.createMaterial(
     new THREE.MeshLambertMaterial({ color: '#dcf2f9', opacity: 0 }),
       .8, // high friction
       .3 // low restitution
-      );
+      )
 
   ground = new Physijs.BoxMesh(
     new THREE.BoxGeometry(100, 1, 100),
     ground_material,
       0 // mass
-      );
-  ground.receiveShadow = true;
-  ground.position.y = -10;
-  scene.add( ground );
+      )
+  ground.receiveShadow = true
+  ground.position.y = -10
+  scene.add( ground )
 
-  spawnBox();
+  group = new THREE.Group()
+  scene.add(group)
 
-  requestAnimationFrame( render );
-  scene.simulate();
-};
+  spawnBox()
+
+  requestAnimationFrame( render )
+  scene.simulate()
+}
+
+function loadModels() {
+  const loader = new THREE.GLTFLoader()
+  const onLoad = ( gltf, position, scale ) => {
+    const model = gltf.scene.children[ 0 ]
+    model.position.copy(position)
+    model.scale.copy(scale)
+    // shapes.push(model)
+    // scene.add( model )
+    group.add(model)
+  }
+
+  const onProgress = () => {}
+  const onError = ( errorMessage ) => { console.log( errorMessage ) }
+
+  const strawberryPosition = new THREE.Vector3(-5, 0, 0)
+  const strawberryScale = new THREE.Vector3(0.07,0.07,0.07)
+  loader.load( 'models/strawberry.glb', gltf => onLoad( gltf, strawberryPosition, strawberryScale ), onProgress, onError )
+}
 
 spawnBox = (function() {
-  // const loader = new THREE.GLTFLoader();
-  // const lemon = loader.load(
-  //   'models/lemon/scene.gltf',
-  //   function ( gltf ) {
-  //     gltf.scene.scale.set(0.2,0.2,0.2)
-  //     scene.add( gltf.scene );
-  //     gltf.animations;
-  //     gltf.scene;
-  //     gltf.scenes;
-  //     gltf.cameras;
-  //     gltf.asset;
-  //   },
-  //   function ( xhr ) {
-  //     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  //   },
-  //   function ( error ) {
-  //     console.log( 'An error happened' );
-  //   }
-  //   ),
 
-  var box_geometry = new THREE.SphereGeometry( 5, 32, 32 ),
+  let sphereGeometry = new THREE.SphereGeometry( 5, 32, 32 ),
 
   createBox = function() {
-    var box, material;
+    let box, material
 
     material = Physijs.createMaterial(
-      new THREE.MeshLambertMaterial({ map: loader.load( 'textures/orange.jpg' ) }),
+      new THREE.MeshLambertMaterial({
+        color: 0xffffff,
+        transparent: true,
+        opacity: 0.3
+      }),
           .6, // medium friction
           .3 // low restitution
-          );
-    material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
-    material.map.repeat.set( .5, .5 );
-
+          )
+    group.add(sphereGeometry)
     box = new Physijs.SphereMesh(
-      box_geometry,
+      sphereGeometry,
       material
-      );
-    box.collisions = 0;
+      )
+    box.collisions = 0
 
     box.position.set(
       Math.random() * 15 - 7.5,
       25,
       Math.random() * 15 - 7.5
-      );
+      )
 
     box.rotation.set(
       Math.random() * Math.PI,
       Math.random() * Math.PI,
       Math.random() * Math.PI
-      );
+      )
 
-    box.castShadow = true;
-    box.addEventListener( 'ready', spawnBox );
-    scene.add( box );
-  };
-
-
+    box.castShadow = true
+    box.addEventListener( 'ready', spawnBox )
+    scene.add( box )
+  }
 
   return function() {
-    setTimeout( createBox, 1000 );
-  };
-})();
+    setTimeout( createBox, 1000 )
+  }
+})()
 
 render = function() {
-  requestAnimationFrame( render );
-  renderer.render( scene, camera );
-};
+  requestAnimationFrame( render )
+  renderer.render( scene, camera )
+}
 
-window.onload = initScene;
+window.onload = initScene
---
+++ group.js
@@ -0,0 +1,159 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  let container
+  let camera
+  let controls
+  let renderer
+  let scene = new THREE.Scene()
+  let shapes = []
+  let group = new THREE.Group()
+
+  // const mixers = []
+  // const clock = new THREE.Clock()
+  function init() {
+    body = document.querySelector('body')
+    // scene = new THREE.Scene()
+    scene.background = new THREE.Color( 0xbffffc )
+
+    createCamera()
+    createControls()
+    createLights()
+    loadModels()
+    createRenderer()
+    animate()
+
+    renderer.setAnimationLoop( () => {
+      update()
+      render()
+    })
+  }
+
+  function createCamera() {
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
+    camera.position.set( -1.5, 1.5, -20 )
+  }
+
+  function createRenderer() {
+    renderer = new THREE.WebGLRenderer()
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    renderer.gammaOutput = true
+    renderer.gammaFactor = 2.2
+    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.physicallyCorrectLights = true
+    body.appendChild( renderer.domElement )
+  }
+
+  function createLights() {
+    const ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 )
+    const mainLight = new THREE.DirectionalLight( 0xffffff, 5 )
+    mainLight.position.set( 10, 10, 10 )
+    scene.add( ambientLight, mainLight )
+  }
+
+  function createControls() {
+    controls = new THREE.OrbitControls( camera, body )
+  }
+
+  const transparentSphere = function() {
+    const geometry = new THREE.SphereGeometry(1.5, 8, 8)
+    const material = new THREE.MeshLambertMaterial({
+      color: 0xffffff,
+      transparent: true,
+      opacity: 0
+    })
+    const mesh = new THREE.Mesh(geometry, material)
+    return mesh
+  }
+
+  scene.add(group)
+
+  function loadModels() {
+    const loader = new THREE.GLTFLoader()
+    const onLoad = ( gltf, position, scale ) => {
+      const model = gltf.scene.children[ 0 ]
+      model.position.copy(position)
+      model.scale.copy(scale)
+      shapes.push(model)
+      group.add(model)
+      // scene.add( model )
+    }
+
+    const onProgress = () => {}
+    const onError = ( errorMessage ) => { console.log( errorMessage ) }
+
+    // const bananaPosition = new THREE.Vector3( 0, 1, 0 )
+    // const bananaScale = new THREE.Vector3(0.01, 0.01, 0.01)
+    // loader.load( 'models/banana.glb', gltf => onLoad( gltf, bananaPosition, bananaScale), onProgress, onError )
+
+    // const lemonPosition = new THREE.Vector3( 7.5, 0, -10)
+    // const lemonScale = new THREE.Vector3(0.8,0.8,0.8)
+    // loader.load( 'models/lemon.glb', gltf => onLoad( gltf, lemonPosition, lemonScale ), onProgress, onError )
+
+    // const orangePosition = new THREE.Vector3( -10, 0, 0 )
+    // const orangeScale = new THREE.Vector3(0.3,0.3,0.3)
+    // loader.load( 'models/orange.glb', gltf => onLoad( gltf, orangePosition, orangeScale ), onProgress, onError )
+
+    // const pearPosition = new THREE.Vector3( 0, 0, 2.5 )
+    // const pearScale = new THREE.Vector3(0.3,0.3,0.3)
+    // loader.load( 'models/pear.glb', gltf => onLoad( gltf, pearPosition, pearScale ), onProgress, onError )
+
+    // const applePosition = new THREE.Vector3( 7.5, 0, -10 )
+    // const appleScale = new THREE.Vector3(0.01, 0.01, 0.01)
+    // loader.load( 'models/apple.glb', gltf => onLoad( gltf, applePosition, appleScale ), onProgress, onError )
+
+    // const pineapplePosition = new THREE.Vector3( 2, 0, 0 )
+    // const pineappleScale = new THREE.Vector3(0.7,0.7,0.7)
+    // loader.load( 'models/pineapple.glb', gltf => onLoad( gltf, pineapplePosition, pineappleScale ), onProgress, onError )
+
+    const cherryPosition = new THREE.Vector3( 5, 0, 0 )
+    const cherryScale = new THREE.Vector3(0.5,0.5,0.5)
+    loader.load( 'models/cherry.glb', gltf => onLoad( gltf, cherryPosition, cherryScale ), onProgress, onError )
+
+    // const strawberryPosition = new THREE.Vector3(-5, 0, 0)
+    // const strawberryScale = new THREE.Vector3(0.07,0.07,0.07)
+    // loader.load( 'models/strawberry.glb', gltf => onLoad( gltf, strawberryPosition, strawberryScale ), onProgress, onError )
+  }
+
+  const sphere = transparentSphere()
+  sphere.position.set(5.5, 0.7, 0)
+  group.add(sphere)
+
+  function update() {
+    // const delta = clock.getDelta();
+    // mixers.forEach( ( mixer ) => { mixer.update( delta ); } );
+  }
+
+  function render() {
+    renderer.render( scene, camera );
+  }
+
+
+  const animate = function(time) {
+    time *= 0.001
+    requestAnimationFrame( animate )
+    camera.lookAt(scene.position)
+    // if (shapes) {
+    //   shapes.forEach(function(shape) {
+    //     shape.position.set(
+    //       Math.cos(time * 1.2) * 5,
+    //       Math.sin(time * 1.1) * 5,
+    //       Math.sin(time * 1.1) * 5
+    //       )
+    //     shape.rotation.x += 0.01
+    //   })
+    // }
+  // renderer.render( scene, camera )
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+}
+
+window.addEventListener( 'resize', onWindowResize )
+init()
+
+
+
+})

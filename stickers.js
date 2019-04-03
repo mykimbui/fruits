284aba2ea3da0f54e0885fc05da9d30330587d1d
My Kim Bui <mykimbuibui@gmail.com>
2019-04-03 15:14:55 +0200

    bg collection

--- stickers.js
+++ stickers.js
@@ -1,18 +1,20 @@
 const grid = document.querySelector('.grid')
 let i = 0
-const colors = ['"red"', '"blue"', '"yellow"', '"green"', '"purple"', '"pink"']
+// const colors = ['"red"', '"blue"', '"yellow"', '"green"', '"purple"', '"pink"']
 
 function placeImage() {
   for (i = 0; i < 31; i++) {
     const nextImage = "images/fruit"+i+".png"
+    const div = document.createElement('div')
+    div.classList.add("bg")
     const img = document.createElement('img')
     img.setAttribute('src', nextImage)
     img.style.width = '100px'
     img.style.margin = '0 auto'
-    img.style.paddingTop = '70px'
-    img.style.paddingBottom = '70px'
-    img.style.backgroudColor = "colors[Math.floor(Math.random() * colors.length)]"
-    grid.appendChild(img)
+    img.style.padding = '70px'
+    img.style.placeItem = 'center'
+    div.appendChild(img)
+    grid.appendChild(div)
   }
 }
 
